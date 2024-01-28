33fd1a8e2675c15fa79e5f8ea1ec0c56d5d9e735
Balaji Shetty Pachai <balaji.pachai08@gmail.com>
2024-01-28 21:42:46 +0530

    completes: first challenge

--- src/1_FuzzThis.sol
+++ src/1_FuzzThis.sol
@@ -2,21 +2,20 @@
 pragma solidity ^0.8.22;
 
 contract FuzzThis {
-    
     bytes32 private immutable secretPassword;
 
     constructor(uint256 _specialNumber) {
         require(_specialNumber < 5000, "Dont make it too hard");
-        secretPassword = keccak256( abi.encode( 15 + _specialNumber ));
+        secretPassword = keccak256(abi.encode(15 + _specialNumber));
     }
 
-    function dontHackMePlease(bytes32 _collectionOfHexadecimal) external returns(bytes memory) {
-        
+    function dontHackMePlease(
+        bytes32 _collectionOfHexadecimal
+    ) external returns (bytes memory) {
         if (_collectionOfHexadecimal == secretPassword) {
             return "win";
         }
 
         return "loss";
-
     }
 }
